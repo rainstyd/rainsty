a70f1683694c6a4154ea06d48a5b433df5807e84
rainsty <1285679912@qq.com>
2019-03-30 19:23:58 +0800

    Submitted for the 3st time at 2019-03-30.

---
+++ Algorithm/DirtyWordOfFilter/__version__.py
@@ -0,0 +1,8 @@
+# __version__.py
+
+__title__ = 'DirtyWordOfFilter'
+__description__ = 'Filter out all dirty words.'
+__version__ = '0.0.1'
+__author__ = 'Residual Mark'
+__alias__ = 'rainsty'
+__author_email__ = '18317509215@163.com'
