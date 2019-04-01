2ce64d1f51be9011d033dfdad5e8952a7d680bc5
rainsty <1285679912@qq.com>
2019-04-01 11:13:43 +0800

    Submitted for the 7st time at 2019-03-30.

--- Algorithm/DirtyWordOfFilter/__version__.py
+++ Algorithm/DirtyWordOfFilter/__version__.py
@@ -1,6 +1,7 @@
 # __version__.py
 
 __title__ = 'DirtyWordOfFilter'
+__date__ = '2019-03-30'
 __description__ = 'Filter out all dirty words.'
 __version__ = '0.0.1'
 __author__ = 'Residual Mark'
--- Algorithm/DirtyWordOfFilter/api.py
+++ Algorithm/DirtyWordOfFilter/api.py
@@ -1,1 +1,39 @@
 # api.py
+
+import os
+import json
+
+HERE = '/'.join(os.path.realpath(__file__).strip().split('/')[:-1])
+
+
+class DFAFilter(object):
+    def __init__(self):
+        self.keyword_chains = {}
+        with open(os.path.join(HERE, 'static', 'WordLibrary.json'), 'r', encoding='utf-8') as f:
+            self.keyword_chains = json.loads(f.read())
+        self.delimit = 'return'
+
+    def filter(self, message, word_re="*"):
+        message = message.lower()
+        ret = []
+        start = 0
+        while start < len(message):
+            level = self.keyword_chains
+            step_ins = 0
+            for char in message[start:]:
+                if char in level:
+                    step_ins += 1
+                    if self.delimit not in level[char]:
+                        level = level[char]
+                    else:
+                        ret.append(word_re * step_ins)
+                        start += step_ins - 1
+                        break
+                else:
+                    ret.append(message[start])
+                    break
+            else:
+                ret.append(message[start])
+            start += 1
+
+        return ''.join(ret)
