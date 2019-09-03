8955e9316aa7f144087fa59a25dd527122b701b1
rainsty <1285679912@qq.com>
2019-09-03 16:20:53 +0800

    spider caipiaoSSQ.py

--- BigData/pandasTask/caipiaoSSQ.py
+++ BigData/pandasTask/caipiaoSSQ.py
@@ -11,13 +11,27 @@ import os
 
 file_path = '../../ScrapyApp/caipiaoSSQ/my_ssq.json'
 save_path = './my_ssq.txt'
-if not os.path.exists(save_path):
-    with open(file_path, 'r') as r, open(save_path, 'w') as w:
-        w.write('one two thr fou fiv six sev')
-        for line in r.readlines():
-            line = str(json.loads(line)['开奖号码']).replace('+', ' ')
-            print(line)
-            w.write('\n' + line)
 
-df = pd.read_csv(save_path)
-print(df)
+
+def ssq_data():
+
+    if not os.path.exists(save_path):
+        with open(file_path, 'r') as r, open(save_path, 'w') as w:
+            w.write('one two thr fou fiv six sev')
+            for line in r.readlines():
+                line = str(json.loads(line)['开奖号码']).replace('+', ' ')
+                print(line)
+                w.write('\n' + line)
+        return 'Make file successful.'
+    else:
+        return 'This file is exists.'
+
+
+def main():
+    print(ssq_data())
+    df = pd.read_csv(save_path)
+    print(df)
+
+
+if __name__ == '__main__':
+    main()
