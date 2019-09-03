8e45f418623f49d5ea18abe9e05b0d2bfb348f4d
rainsty <1285679912@qq.com>
2019-09-03 22:22:48 +0800

    xpath-basis.py

--- Spiders/xpath-basis.py
+++ Spiders/xpath-basis.py
@@ -1,27 +1,55 @@
 #!/usr/bin/python
 # encoding: utf-8
 
+import requests
+from lxml import etree
+
 """
 @author: rainsty
 @file:   xpath-basis.py
 @time:   2019-09-03 17:42
 @description:
 """
-from lxml import etree
 
-text = '''
-<div>
-   <ul>
-     <li name="wjk" class="two">wjk</li>
-    <li name="wang" class="three">wang</li>
-    <li name="karry" class="four">karry</li>
-   </ul>
-</div>
-'''
+"""Example
+    from lxml import etree
+    
+    text = '''
+    <div>
+       <ul>
+         <li name="wjk" class="two">wjk</li>
+        <li name="wang" class="three">wang</li>
+        <li name="karry" class="four">karry</li>
+       </ul>
+    </div>
+    '''
+    
+    html = etree.HTML(text)
+    results = html.xpath('//li')
+    print(results)
+    for r in results:
+        print(r.tag)
+        print(r.text)
+        print(r.attrib)
 
-html = etree.HTML(text)
-results = html.xpath('//li')
-for r in results:
-    print(r.tag)
-    print(r.text)
-    print(r.attrib)
+"""
+
+
+def get_html_xpath(xpath):
+    url = 'http://www.jinxiaoke.com/'
+    resp = requests.get(url)
+    code = resp.encoding
+    html = etree.HTML(resp.text)
+    results = html.xpath(xpath)
+    return [(r.text.encode(code).decode('utf-8'), r.attrib) for r in results if r.text is not None]
+
+
+def main():
+    print(get_html_xpath('/html/body/div[1]/section[5]/div[@class="container"]/div[1]/div[2]//p'))
+    print(get_html_xpath('/html/body/div[1]/section[5]/div[@class="container"]/div[1]/div[2]/p'))
+    print(get_html_xpath('/html/body/div[1]/section[5]/div[@class="container"]/div[1]/div[2]/p[last()-2]'))
+    print(get_html_xpath('/html/body/div[1]/section[5]/div[@class="container"]/div[1]/div[2]/p[position()<2]'))
+
+
+if __name__ == '__main__':
+    main()
