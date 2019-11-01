b72bee58a0f3ebe0c53b8e24302545aea1cd9583
Residual Mark <1285679912@qq.com>
2019-11-01 21:56:05 +0800

    update files

---
+++ InstallSoft/docker/install_ubuntu.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt-get remove docker docker-engine docker.io
+sudo apt-get update
+sudo apt-get install apt-transport-https ca-certificates curl gnupg2 software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install docker-ce
