f0c6d5c6face691fa592c6ad9432cc0a87423eb9
Residual Mark <1285679912@qq.com>
2019-11-04 09:16:01 +0800

    pycanal

---
+++ Modules/pycanal/src/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+@author: rainsty
+@file:   __init__.py
+@time:   2019-11-03 20:45:48
+@description:
+"""
+
+from __future__ import absolute_import
+
+from .api import *
+
+__version__ = '1.0.0'
+__license__ = ''
+
