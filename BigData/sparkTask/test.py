68b1e5a94ba14e3c638d9eda3a7f9701b663af21
郭鑫瑞 <guoxinrui@tzt.cn>
2020-01-04 20:07:49 +0800

    falcon

--- BigData/sparkTask/test.py
+++ BigData/sparkTask/test.py
@@ -11,29 +11,30 @@
 import os
 from pyspark.sql import SparkSession
 
-os.environ['JAVA_HOME']='/root/jdk'
-os.environ['PYTHON_HOME']="/root/python"
-os.environ['PYSPARK_PYTHON']="/usr/bin/python"
-os.environ['SPARK_HOME']='/root/spark'
-os.environ['SPARK_MASTER_IP']='rainsty'
+os.environ['JAVA_HOME'] = '/root/jdk'
+os.environ['PYTHON_HOME'] = "/root/python"
+os.environ['PYSPARK_PYTHON'] = "/usr/bin/python"
+os.environ['SPARK_HOME'] = '/root/spark'
+os.environ['SPARK_MASTER_IP'] = '47.99.139.144'
 
-def CreateSparkContext():
 
-    spark = SparkSession.builder \
+def create_spark_context():
+
+    sc = SparkSession.builder \
         .appName("TestSparkSession") \
-        .master("spark://rainsty:7077") \
-        .config('spark.executor.num','1')\
-        .config('spark.executor.memory','512m')\
-        .config("spark.executor.cores",'1')\
-        .config('spark.cores.max','1')\
-        .config('spark.driver.memory','512m') \
+        .master("spark://47.99.139.144:7077") \
+        .config('spark.executor.num', '1')\
+        .config('spark.executor.memory', '512m')\
+        .config("spark.executor.cores", '1')\
+        .config('spark.cores.max', '1')\
+        .config('spark.driver.memory', '512m') \
         .getOrCreate()
 
-    return spark
+    return sc
 
 
 logFile = "/root/spark/README.md"
-spark = CreateSparkContext()
+spark = create_spark_context()
 logData = spark.read.text(logFile).cache()
 
 numAs = logData.filter(logData.value.contains('a')).count()
@@ -42,4 +43,3 @@ numBs = logData.filter(logData.value.contains('b')).count()
 print("Lines with a: %i, lines with b: %i" % (numAs, numBs))
 
 spark.stop()
-
