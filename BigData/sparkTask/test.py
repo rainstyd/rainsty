8bd39b7632ca7e155b616091c30efbbec1076b81
Residual Mark <1285679912@qq.com>
2020-01-04 19:14:02 +0800

    spark

---
+++ BigData/sparkTask/test.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+@author: rainsty
+@file:   test.py
+@time:   2020-01-04 18:36:57
+@description:
+"""
+
+import os
+from pyspark.sql import SparkSession
+
+os.environ['JAVA_HOME']='/root/jdk'
+os.environ['PYTHON_HOME']="/root/python"
+os.environ['PYSPARK_PYTHON']="/usr/bin/python"
+os.environ['SPARK_HOME']='/root/spark'
+os.environ['SPARK_MASTER_IP']='rainsty'
+
+def CreateSparkContext():
+
+    spark = SparkSession.builder \
+        .appName("TestSparkSession") \
+        .master("spark://rainsty:7077") \
+        .config('spark.executor.num','1')\
+        .config('spark.executor.memory','512m')\
+        .config("spark.executor.cores",'1')\
+        .config('spark.cores.max','1')\
+        .config('spark.driver.memory','512m') \
+        .getOrCreate()
+
+    return spark
+
+
+logFile = "/root/spark/README.md"
+spark = CreateSparkContext()
+logData = spark.read.text(logFile).cache()
+
+numAs = logData.filter(logData.value.contains('a')).count()
+numBs = logData.filter(logData.value.contains('b')).count()
+
+print("Lines with a: %i, lines with b: %i" % (numAs, numBs))
+
+spark.stop()
+
