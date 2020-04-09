d74a8fe35fd68775dabf794e551982408fd2b488
郭鑫瑞 <guoxinrui@tzt.cn>
2020-04-09 17:25:11 +0800

    flask

--- Docker/flaskAppCompose/app.sh
+++ Docker/flaskAppCompose/app.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 function main(){
-    pip3 install --upgrade pip
-    cat /home/flaskApp/requirements.txt|xargs pip3 install -i http://mirrors.aliyun.com/pypi/simple/
+    # pip3 install --upgrade pip
+    cat /home/flaskApp/requirements.txt|xargs pip3 install -i http://mirrors.aliyun.com/pypi/simple/ --trusted-host mirrors.aliyun.com
     chmod +x /home/flaskApp/rainDB/bin rainDB-shell
     cd /home/flaskApp
     bash startApp.sh
