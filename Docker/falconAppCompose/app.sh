57e6528df26db06ecd387a54b4f1f15572a5161f
郭鑫瑞 <guoxinrui@tzt.cn>
2020-04-15 13:49:39 +0800

    falcon

--- Docker/falconAppCompose/app.sh
+++ Docker/falconAppCompose/app.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 function main(){
-    pip install --upgrade pip
+    # pip install --upgrade pip
     cat /home/falconApp/requirements.txt|xargs pip install -i http://mirrors.aliyun.com/pypi/simple/ --trusted-host mirrors.aliyun.com
     cd /home/falconApp
     nohup gunicorn -w 2 manage_app:app -b 0.0.0.0:5000 --timeout 1800 &
--- Docker/flaskAppCompose/app.sh
+++ Docker/flaskAppCompose/app.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 function main(){
-    pip install --upgrade pip
+    # pip install --upgrade pip
     cat /home/flaskApp/requirements.txt|xargs pip install -i http://mirrors.aliyun.com/pypi/simple/ --trusted-host mirrors.aliyun.com
     chmod +x /home/flaskApp/rainDB/bin/rainDB-shell
     cd /home/flaskApp
