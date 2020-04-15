9732e4fbe0f74a6e1b1c573a573b913a3b69ebc5
郭鑫瑞 <guoxinrui@tzt.cn>
2020-04-15 15:00:11 +0800

    falcon

--- Docker/falconAppCompose/app.sh
+++ Docker/falconAppCompose/app.sh
@@ -4,7 +4,7 @@ function main(){
     # pip install --upgrade pip
     cat /home/falconApp/requirements.txt|xargs pip install -i http://mirrors.aliyun.com/pypi/simple/ --trusted-host mirrors.aliyun.com
     cd /home/falconApp
-    nohup gunicorn -w 2 manage_app:app -b 0.0.0.0:5000 --timeout 1800 &
+    gunicorn -w 2 manage_app:app -b 0.0.0.0:5000 --timeout 1800
 }
 
 main
