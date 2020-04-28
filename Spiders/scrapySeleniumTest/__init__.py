aa7a8ac997f56afe279e761b693d3cdb7326fbda
郭鑫瑞 <guoxinrui@tzt.cn>
2020-04-28 14:05:13 +0800

    falcon

---
+++ Spiders/scrapySeleniumTest/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+@author: rainsty
+@file:   __init__.py
+@time:   2020-04-28 14:02:29
+@description:
+"""
+
+"""
+mkdir scrapySeleniumTest
+cd scrapySeleniumTest
+scrapy startproject caipiaoSSQ
+cd caipiaoSSQ
+scrapy genspider ssq 17500.cn
+"""
