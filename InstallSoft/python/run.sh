2d9ed4ea89c691611c82aa4a4e61033622bafb64
郭鑫瑞 <guoxinrui@tzt.cn>
2020-08-07 17:54:18 +0800

    rainsty

---
+++ InstallSoft/python/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+check_ll=`cat /root/.bashrc |grep "^alias ll" | awk -F '=' '{print $1}'`
+
+if ![ -n "${check_ll}" ]; then
+    sed -i "s/# alias ll='ls \$LS_OPTIONS -l'/alias ll='ls \$LS_OPTIONS -l'/g" /root/.bashrc
+    source /root/.bashrc
+fi
+
+pip install --upgrade pip -i http://mirrors.aliyun.com/pypi/simple --trusted-host mirrors.aliyun.com
+pip install -r requirements.txt -i http://mirrors.aliyun.com/pypi/simple --trusted-host mirrors.aliyun.com
+
+tail -f /dev/null
