be3bde9254a2836f9a6567f0f16fb3f7469c62e7
郭鑫瑞 <guoxinrui@tzt.cn>
2020-09-16 14:00:53 +0800

    python

---
+++ Docs/dockerFile/python/volumes/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+check_ll=`cat /root/.bashrc |grep "^alias ll" | awk -F '=' '{print $1}'`
+echo ${check_ll}
+
+if ! [ -n "${check_ll}" ]; then
+    sed -i "s/# alias ll='ls \$LS_OPTIONS -l'/alias ll='ls \$LS_OPTIONS -l'/g" /root/.bashrc
+    source /root/.bashrc
+fi
+
+pip install --upgrade pip -i http://mirrors.aliyun.com/pypi/simple --trusted-host mirrors.aliyun.com
+pip install -r requirements.txt -i http://mirrors.aliyun.com/pypi/simple --trusted-host mirrors.aliyun.com
+
+tail -f /dev/null
