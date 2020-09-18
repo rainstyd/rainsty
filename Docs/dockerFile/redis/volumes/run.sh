46c72bb1fb804241d5a8b7551b9264f0392a0baf
郭鑫瑞 <guoxinrui@tzt.cn>
2020-09-18 10:08:54 +0800

    python

--- Docs/dockerFile/redis/volumes/run.sh
+++ Docs/dockerFile/redis/volumes/run.sh
@@ -1,12 +1,40 @@
 #!/bin/bash
 
-check_ll=`cat /root/.bashrc |grep "^alias ll" | awk -F '=' '{print $1}'`
-echo ${check_ll}
 
-if ! [ -n "${check_ll}" ]; then
-    sed -i "s/# alias ll='ls \$LS_OPTIONS -l'/alias ll='ls \$LS_OPTIONS -l'/g" /root/.bashrc
-    source /root/.bashrc
-fi
+function checkSelf {
+    check_ll=`cat /root/.bashrc |grep "^alias ll" | awk -F '=' '{print $1}'`
 
-# port password 开启持久化
-redis-server --port 6379 --requirepass 123456  --appendonly yes
+    if ! [ -n "${check_ll}" ]; then
+        sed -i "s/# alias ll='ls \$LS_OPTIONS -l'/alias ll='ls \$LS_OPTIONS -l'/g" /root/.bashrc
+        source /root/.bashrc
+    fi
+}
+
+
+function envSelf {
+    if [[ -z "$REDIS_PORT" ]]; then
+        export $REDIS_PORT="6379"
+    fi
+
+    if [[ -z "$REDIS_PASSWORD" ]]; then
+        export REDIS_PASSWORD="123456"
+    fi
+
+    if [[ -z "$REDIS_APPENDONLY" ]]; then
+        export REDIS_APPENDONLY="yes"
+    fi
+}
+
+
+function runSelf {
+    redis-server --port $REDIS_PORT --requirepass $REDIS_PASSWORD  --appendonly $REDIS_APPENDONLY
+}
+
+
+function main {
+    checkSelf
+    envSelf
+}
+
+
+main
