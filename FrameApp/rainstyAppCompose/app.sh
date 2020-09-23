f6eca4724006365f43ce441851110e0be6b491df
郭鑫瑞 <guoxinrui@tzt.cn>
2020-09-23 11:30:57 +0800

    python

--- FrameApp/rainstyAppCompose/app.sh
+++ FrameApp/rainstyAppCompose/app.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 function main(){
-    # pip install --upgrade pip
+    pip install --upgrade pip -i http://mirrors.aliyun.com/pypi/simple/ --trusted-host mirrors.aliyun.com
     cat /home/rainstyApp/requirements.txt|xargs pip install -i http://mirrors.aliyun.com/pypi/simple/ --trusted-host mirrors.aliyun.com
     cd /home/rainstyApp && bash app_start.sh
 }
