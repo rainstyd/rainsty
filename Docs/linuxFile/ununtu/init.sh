53aa0da24d11b23984de6dfd24158f97cb9054e2
rainsty <rainstyd@sina.com>
2023-11-16 13:49:29 +0800

    python

--- Docs/linuxFile/ununtu/init.sh
+++ Docs/linuxFile/ununtu/init.sh
@@ -5,11 +5,19 @@ su root
 apt-get install ssh -y
 apt-get install vim -y
 vim /etc/ssh/sshd_config
-# :28 跳转到28行
+# :28 跳转到28行 PermitRootLogin 取消该行注释，将prohibit-password修改为yes
 vim /etc/hostname
 
 cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 apt-get remove iptables -y
+
+sudo vi /etc/network/interfaces
+
+iface ens33 inet static
+address 192.168.1.163
+netmask 255.255.255.0
+gateway 192.168.1.1
+dns-nameservers 114.114.114.114
 
 reboot
 
@@ -23,13 +31,20 @@ sudo apt-get update -y
 sudo apt-get install docker.io -y
 
 bash install_docker.sh
+apt-get install docker-compose -y
 
 vi /etc/docker/daemon.json
 {
     "registry-mirrors": ["https://hub-mirror.c.163.com"],
     "insecure-registries":["127.0.0.1:8088"]
 }
-
+{
+    "insecure-registries":
+    [
+          "repo.tzt.cn:8088",
+          "repo.tzt.cn:8085"
+    ]
+}
 systemctl daemon-reload && systemctl restart docker
 docker pull 127.0.0.1:8088/alarm/zookeeper:v1
 
